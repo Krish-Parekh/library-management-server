b069d21e9297e1074d1072e96eb3516dfa474fd5
Krish-Parekh <krishparekh303@gmail.com>
2024-06-03 15:01:33 +0530

    feat(core): adding authorization to admin route

--- router/user.router.js
+++ router/user.router.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import {
-    getAllUser
+    getAllUser,
+    getUser,
+    deleteUser
 } from "../controller/user.controller.js";
+import { authorize } from "../middleware/role.middleware.js";
 
 const router = Router();
 
-router.route("/").get(getAllUser);
-
+router.route("/").get(authorize("admin"), getAllUser);
+router.route("/:id").get(authorize("admin"), getUser)
+router.route("/:id").delete(authorize("admin"), deleteUser);    
 export default router;
