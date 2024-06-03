0af548b0474294a27d6dec56376ceb2b4e7be4e8
Krish-Parekh <krishparekh303@gmail.com>
2024-06-03 09:19:00 +0530

    feat(core): https certified

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 import { connectDB } from "./lib/db.js";
+import fs from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
-
 dotenv.config();
 app.use(express.json());
 app.use(cors("*"));
@@ -24,6 +30,10 @@ import { verifyToken } from "./middleware/auth.middleware.js";
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Server is running" });
 });
+
+app.get("/.well-known/pki-validation/3CE78B620E7B3C6A4C4C814502D974F8.txt", (req, res) => {
+ res.sendFile(path.join(__dirname, '3CE78B620E7B3C6A4C4C814502D974F8.txt')); 
+})
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", verifyToken, userRouter);
